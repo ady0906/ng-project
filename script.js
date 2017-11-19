a39f7e06aea9d5144d59e6a288f8e71358823eb0
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-19 17:10:51 -0500

    cleaning script

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-//
-// var MainController = function($scope, $http) {
-//
-//     var onUserComplete = function(response) {
-//         $scope.user = response.data
-//     }
-//     $scope.message = "Hello, Angular!";
-// };
-
 (function() {
     var myApp = angular.module("myApp", []);
     myApp.controller("MainController", ["$scope", function($scope) {
