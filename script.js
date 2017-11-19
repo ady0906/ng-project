77e23258091a3299b8645c3c3bab51c594660d3d
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-18 21:57:16 -0500

    fixing up ng syntax

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+//
+// var MainController = function($scope, $http) {
+//
+//     var onUserComplete = function(response) {
+//         $scope.user = response.data
+//     }
+//     $scope.message = "Hello, Angular!";
+// };
 
-var MainController = function($scope, $http) {
-
-    var onUserComplete = function(response) {
-        $scope.user = response.data
-    }
-    $scope.message = "Hello, Angular!";
-};
+(function() {
+    angular.module('myApp', []).controller('MainController', function($scope) {
+            var onUserComplete = function(response) {
+                $scope.user = response.data
+            }
+        $scope.message = "Hello, Angular!";
+    });
+}());
