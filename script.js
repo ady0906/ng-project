d6dcfa3b377bbb88a7ed9220e39809a241e688e4
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-20 13:43:09 -0500

    setting up http in controller

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 (function() {
     var myApp = angular.module("myApp", []);
-    myApp.controller("MainController", ["$scope", function($scope) {
-            // var onUserComplete = function(response) {
-            //     $scope.user = response.data
-            // }
+    myApp.controller("MainController", function($scope, $http) {
+
+        var onUserComplete = function(response) {
+            $scope.user = response.data;
+        };
+
+        $http.get("https://api.github.com/users/robconery")
+             .then(onUserComplete);
         $scope.message = "Hello, Angular!";
-    }]);
+    });
 }());
