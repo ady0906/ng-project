bb5713730cd9cf0fbeee194b1dd274b512e78e4b
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-20 13:50:05 -0500

    adding onError function

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 (function() {
     var myApp = angular.module("myApp", []);
+    // Passing in array of dependencies
     myApp.controller("MainController", function($scope, $http) {
 
         var onUserComplete = function(response) {
             $scope.user = response.data;
         };
 
+        var onError = function(reason) {
+            $scope.error = "Could not fetch the user";
+        }
+
         $http.get("https://api.github.com/users/robconery")
-             .then(onUserComplete);
+             .then(onUserComplete, onError);
         $scope.message = "Hello, Angular!";
     });
 }());
