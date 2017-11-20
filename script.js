eca70a4eb5cfde5a20fff0857e2e7bfa0cb3a811
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-20 15:14:22 -0500

    using ng-repeat for repos infos

--- script.js
+++ script.js
@@ -3,24 +3,29 @@
     // Passing in array of dependencies
 
     var MainController = function($scope, $http) {
-
         var onUserComplete = function(response) {
             $scope.user = response.data;
+            $http
+                .get($scope.user.repos_url)
+                .then(onRepos, onError);
+        };
+
+        var onRepos = function(response) {
+            $scope.repos = response.data;
         };
 
         var onError = function(reason) {
-            $scope.error = 'Could not fetch the user';
+            $scope.error = 'Could not fetch the data.';
         };
 
         $scope.search = function(username) {
             $http
-            .get('https://api.github.com/users/' + username)
-            .then(onUserComplete, onError);
-        }
-        $scope.username = "angular";
-        $scope.message = "GitHub viewer";
-        
-    }
+                .get('https://api.github.com/users/' + username)
+                .then(onUserComplete, onError);
+        };
+        $scope.username = 'angular';
+        $scope.message = 'GitHub viewer';
+    };
 
-    myApp.controller("MainController", ["$scope", "$http", MainController]);
+    myApp.controller('MainController', ['$scope', '$http', MainController]);
 })();
