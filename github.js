31f9d752c235c286073d1d6036e3727a1d5664e0
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-23 12:59:20 -0500

    requiring http service in github service

--- github.js
+++ github.js
@@ -1,4 +1,9 @@
 (function(){
+    var github = function($http) {
+        return {
+
+        };
+    };
     // Getting reference to exisiting module
     var module = angular.module("githubViewer");
 
