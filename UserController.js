5d998f3427b349fb38a742b2d03229d435fc2919
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-24 13:40:32 -0500

    cleaning up UserController file

--- UserController.js
+++ UserController.js
@@ -2,14 +2,7 @@
     var myApp = angular.module('myApp');
     // Passing in array of dependencies
 
-    var MainController = function(
-        $scope,
-        github,
-        $interval,
-        $log,
-        $anchorScroll,
-        $location
-    ) {
+    var UserController = function($scope, github, $routeParams) {
         var onUserComplete = function(data) {
             $scope.user = data;
             github.getRepos($scope.user).then(onRepos, onError);
@@ -25,43 +18,9 @@
             $scope.error = 'Could not fetch the data.';
         };
 
-        var decrementCountdown = function() {
-            $scope.countdown -= 1;
-            if ($scope.countdown < 1) {
-                $scope.search($scope.username);
-            }
-        };
-        var countdownInterval = null;
-        var startCountdown = function() {
-            countdownInterval = $interval(
-                decrementCountdown,
-                1000,
-                $scope.countdown
-            );
-        };
-
-        $scope.search = function(username) {
-            $log.info('Searching for ' + username);
-            github.getUser(username).then(onUserComplete, onError);
-            if (countdownInterval) {
-                $interval.cancel(countdownInterval);
-                $scope.countdown = null;
-            }
-        };
         $scope.username = 'angular';
-        $scope.message = 'GitHub viewer';
         $scope.repoSortOrder = '-stargazers_count';
-        $scope.countdown = 5;
-        startCountdown();
     };
 
-    myApp.controller('MainController', [
-        '$scope',
-        'github',
-        '$interval',
-        '$log',
-        '$anchorScroll',
-        '$location',
-        MainController,
-    ]);
+    myApp.controller('UserController', UserController);
 })();
