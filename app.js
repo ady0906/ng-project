4dc383a0e8914010c99464c88c55a6c74edd9c58
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-27 11:43:24 -0500

    setting up new route in app.js file

--- app.js
+++ app.js
@@ -12,6 +12,13 @@
                 templateUrl: "user.html",
                 controller: "UserController"
             })
+            .when("/repo/:username/:reponame", {
+                templateUrl: "repo.html",
+                controller: "RepoController"
+            })
+            // 2 http calls needed
+                // api.github.com/repos/username/reponame
+                // Get collaborators with api.github.com/repos/username/reponame/contributors
             .otherwise({redirectTo: "/main"});
     });
 
