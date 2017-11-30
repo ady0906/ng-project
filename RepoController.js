07f9c510c0439234937eaaa264416b54d9d3e98e
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-30 12:37:21 -0500

    setting onError function

--- RepoController.js
+++ RepoController.js
@@ -5,7 +5,12 @@
     var RepoController = function($scope, github, $routeParams) {
         onRepoComplete = function(data) {
             // $scope.user =
-        }
+        };
+
+        var onError = function(reason) {
+            $scope.error = 'Could not fetch the data.';
+        };
+
         $scope.username = $routeParams.username;
         $scope.reponame = $routeParams.reponame;
     };
