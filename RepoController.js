d26390a85690a754a21cb31801644014e369e5f3
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-12-01 13:33:53 -0500

    putting reponame up on repo view + receiving data

--- RepoController.js
+++ RepoController.js
@@ -4,8 +4,19 @@
 
     var RepoController = function($scope, $routeParams, $log, github) {
         onRepoComplete = function(data) {
-            // $scope.user =
+            console.log(data);
+            // $scope.repo = data;
+            // github.getContributors($scope.)
         }
+
+        // var onUserComplete = function(data) {
+        //     $scope.user = data;
+        //     github.getRepos($scope.user).then(onRepos, onError);
+        // };
+        //
+        // var onRepos = function(data) {
+        //     $scope.repos = data;
+        // };
 
         var onError = function(reason) {
             $scope.error = 'Could not fetch the data.';
@@ -13,8 +24,9 @@
 
         $scope.username = $routeParams.username;
         $scope.reponame = $routeParams.reponame;
-        $scope.name = 'zboob';
-        $scope.$log = 'Repo de ouf';
+        github.getRepo($scope.username, $scope.reponame).then(onRepoComplete, onError);
+        // $scope.name = 'zboob';
+        // $scope.$log = 'Repo de ouf';
     };
 
     myApp.controller('RepoController', RepoController);
